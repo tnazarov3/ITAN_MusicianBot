487c3995a2761773eeedea010e2d3363fba252ae
Тимофей <tima.nazarov.03.05.2003@gmail.ru>
2024-04-02 07:22:07 +0400

    improved feedback from bot

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
 from discord.ext import commands
-import yt_dlp as youtube_dl
+from yt_dlp import YoutubeDL
+from pytube import Playlist
 import discord
 import asyncio
+import pytube
 import dotenv
 import os
 
-global queue, i, play_flag
+global queue, i, play_flag, video_title_name, playlist_added_msg, first_added_of_pl, playlist_urls, dwnld_pl_flag
 
 old_files = os.listdir('D:/ITAN/')
 for fil in range(len(old_files)):
@@ -15,19 +17,6 @@ intents = discord.Intents().all()
 client = discord.Client(intents=intents)
 bot = commands.Bot(command_prefix='!', intents=intents)
 
-ydl_opts = {
-    'format': 'bestaudio/best',
-    'outtmpl': 'D:/ITAN/%(title)s.%(ext)s',
-    'restrictfilenames': True,
-    'nocheckcertificate': True,
-    'ignoreerrors': True,
-    'logtostderr': False,
-    'quiet': True,
-    'no_warnings': True,
-    'default_search': 'auto',
-    'source_address': '0.0.0.0',
-}
-ydl = youtube_dl.YoutubeDL(ydl_opts)
 ffmpeg_options = {'options': '-vn'}
 
 
@@ -47,7 +36,7 @@ async def play_song(ctx, url):
     except NameError:
         queue = []
         i = 0
-    print(queue)
+
     try:
         server = ctx.message.guild
         voice_channel = server.voice_client
@@ -66,57 +55,112 @@ async def play_song(ctx, url):
 
 
 async def start_playing(voice_channel, ctx):
-    global i, play_flag
+    global i, play_flag, dwnld_pl_flag
     while True:
         if play_flag:
             global queue
             try:
                 voice_channel.play(discord.FFmpegPCMAudio(executable="ffmpeg.exe", source=f'D:/ITAN/{queue[i]}'))
                 await ctx.send(f'**Пою: **`{queue[i]}`'.replace('.webm', '').replace('_', ' '))
-                i += 1
-                try: os.remove(queue[i-1]) if i > 0 else 0
+                try:
+                    os.remove(f'D:/ITAN/{queue[i-1]}')
                 except: pass
-            except: pass
+                i += 1
+            except:
+                if dwnld_pl_flag:
+                    try:
+                        await download_playlist()
+                    except: pass
+                else: pass
+
             await asyncio.sleep(3)
+
         elif not play_flag:
             break
 
 
 async def download(url, ctx):
-    global queue
-    global dwnld_msg, added
-    added = None
+    global queue, playlist_urls, first_of_pl, playlist_added_msg
+    global dwnld_msg_text, added, first_added_of_pl, dwnld_pl_flag
+
     if '/playlist' in url:
-        added = []
-        ydl.download([url])
+        dwnld_pl_flag = True
+        playlist_urls = Playlist(url)
+        first_of_pl = [playlist_urls[0], pytube.YouTube(playlist_urls[0]).streams[0].title]
+        YoutubeDL(set_video_name(first_of_pl[1])).download(url)
+
         files = os.listdir('D:/ITAN/')
         for n in range(len(files)):
             if files[n] not in queue:
                 queue.append(files[n])
-                added.append(files[n])
+                first_added_of_pl = files[n]
             else:
                 pass
-        dwnld_msg = f'**Плейлист добавлен**\n```{added}```'
-        dwnld_msg = dwnld_msg.replace('[', '-').replace(']', '')
-        dwnld_msg = dwnld_msg.replace("'", '').replace('_', ' ')
-        dwnld_msg = dwnld_msg.replace('.webm', '').replace(', ', '\n-')
+        dwnld_msg_text_pl = f'**Добавлен плейлист: **`\n-{first_added_of_pl}`'.replace('.webm', '\n...').replace('_', ' ')
+        playlist_added_msg = await ctx.send(dwnld_msg_text_pl)
 
     else:
-        ydl.download([url])
+        added = None
+        video_title_name = pytube.YouTube(url).streams[0].title
+        YoutubeDL(set_video_name(video_title_name)).download(url)
         files = os.listdir('D:/ITAN/')
         for n in range(len(files)):
             if files[n] not in queue:
                 queue.append(files[n])
                 added = files[n]
-                dwnld_msg = f'**Добавлен: **`{added}`'
-                dwnld_msg = dwnld_msg.replace('.webm', '').replace('_', ' ')
+                dwnld_msg_text = (f'**Добавлен: **`{added}`'.replace('.webm', '').replace('_', ' '))
             else:
                 pass
 
         if added is None:
-            dwnld_msg = '**Трек уже в очереди**'
+            dwnld_msg_text = '**Трек уже в очереди**'
 
-    await ctx.send(dwnld_msg)
+        await ctx.send(dwnld_msg_text)
+
+
+async def download_playlist():
+    global playlist_added_msg, queue, dwnld_pl_flag, first_added_of_pl
+    pl_urls = []
+    for url in playlist_urls:
+        pl_urls.append(url)
+    added = []
+    for url in pl_urls[1:]:
+        try:
+            name = pytube.YouTube(url).streams[0].title
+            YoutubeDL(set_video_name(name)).download(url)
+
+            files = os.listdir('D:/ITAN/')
+            for n in range(len(files)):
+                if files[n] not in queue:
+                    queue.append(files[n])
+                    added.append(files[n])
+                else:
+                    pass
+            if added:
+                text = f'**Плейлист добавлен**\n```-{first_added_of_pl}\n{added}```'
+                text = text.replace("', '", '\n-')
+                text = text.replace("['", '-').replace("']", '')
+                text = text.replace('_', ' ').replace('.webm', '')
+                await playlist_added_msg.edit(content=text)
+        except Exception as err:print('1 ', err)
+
+    dwnld_pl_flag = False
+
+
+def set_video_name(video_name):
+    ydl_opts = {
+        'format': 'bestaudio/best',
+        'outtmpl': f'D:/ITAN/{video_name}.%(ext)s',
+        'restrictfilenames': True,
+        'nocheckcertificate': True,
+        'ignoreerrors': True,
+        'logtostderr': False,
+        'quiet': True,
+        'no_warnings': True,
+        'default_search': 'auto',
+        'source_address': '0.0.0.0',
+    }
+    return ydl_opts
 
 
 @bot.command(name='дауби', help='Остановать/Продолжить')
@@ -140,6 +184,8 @@ async def next_track(ctx):
     if voice_client.is_playing():
         await ctx.send('**да есть же**')
         voice_client.stop()
+    else:
+        await ctx.send('**В очереди нет треков**')
 
 
 @bot.command(name='яколян', help='Остановить и сбросить очередь')
@@ -165,9 +211,9 @@ async def print_queue(ctx):
     global queue, i
     if len(queue[i:]) > 0:
         queue_msg = f'**Дальше по списку: **```{queue[i:]}```'
-        queue_msg = queue_msg.replace('.webm', f'').replace(', ', '\n-')
-        queue_msg = queue_msg.replace('[', '-').replace(']', '')
-        queue_msg = queue_msg.replace("'", '').replace('_', ' ')
+        queue_msg = queue_msg.replace("', '", '\n-')
+        queue_msg = queue_msg.replace("['", '-').replace("']", '')
+        queue_msg = queue_msg.replace('_', ' ').replace('.webm', '')
         await ctx.send(queue_msg)
     else:
         queue_msg = '**В очереди нет треков**'
